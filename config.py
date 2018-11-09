af5537d6716a71e357576993c968cef580316d0d
mwanduka <mwandukastephen20@gmail.com>
2018-11-09 12:55:26 +0300

    add new files

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ class Config:
     '''
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+
+
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
